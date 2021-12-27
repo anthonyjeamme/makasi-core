2ffdb28fcdf10ad58f4fbfeedd2aa1f580cca547
anthonyjeamme <anthony@jeamme.fr>
2021-12-27 21:09:59 +0100

    update Connector type

--- src/Connector/Connector.types.ts
+++ src/Connector/Connector.types.ts
@@ -3,9 +3,9 @@ import { TPageData } from '../Page/Page.types'
 export type TConnector = {
   getPage: (id: string) => Promise<TPageData>
   getPages: (type?: string) => Promise<TPageData[]>
-  updatePage: (id: string, data: any) => Promise<void>
+  updatePage: (id: string, data: any) => Promise<TPageData>
   removePage: (id: string) => Promise<void>
-  addPage: (data: any) => Promise<void>
+  addPage: (data: any) => Promise<TPageData>
 }
 
 export type TConnectorFactory = (id: string) => TConnector
