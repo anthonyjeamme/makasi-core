22a4859134a85f70cc14421e6444ae0e563d28b8
anthonyjeamme <anthony@jeamme.fr>
2021-12-29 17:52:55 +0100

    refactorize page -> add Page Component

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -6,6 +6,43 @@ import { PageContent } from './PageContent'
 
 import { PageDefinition } from './PageDefinition'
 
+export const Page = ({
+  pageDefinition,
+  pageId,
+  staticData = null,
+  pageParams = {}
+}) => {
+  const [data, setData] = useState(staticData)
+
+  const { connector, adminTheme } = useMakasiContext()
+
+  useEffect(() => {
+    if (!staticData && connector)
+      connector.getPage(pageId).then((data) => {
+        setData(data || pageDefinition.defaultData)
+      })
+  }, [])
+
+  if (data === null) return null
+
+  return (
+    <PageContextProvider
+      pageDefinition={pageDefinition}
+      pageData={data}
+      pageId={pageId}
+      pageParams={pageParams}
+    >
+      {(pageData) => (
+        <PageContent
+          data={pageData}
+          dataConnector={connector}
+          theme={adminTheme}
+        />
+      )}
+    </PageContextProvider>
+  )
+}
+
 export const page =
   (
     pageDefinition: PageDefinition,
@@ -14,33 +51,12 @@ export const page =
     pageParams = {}
   ) =>
   () => {
-    const [data, setData] = useState(staticData)
-
-    const { connector, adminTheme } = useMakasiContext()
-
-    useEffect(() => {
-      if (!staticData && connector)
-        connector.getPage(pageId).then((data) => {
-          setData(data || pageDefinition.defaultData)
-        })
-    }, [])
-
-    if (data === null) return null
-
     return (
-      <PageContextProvider
+      <Page
         pageDefinition={pageDefinition}
-        pageData={data}
         pageId={pageId}
+        staticData={staticData}
         pageParams={pageParams}
-      >
-        {(pageData) => (
-          <PageContent
-            data={pageData}
-            dataConnector={connector}
-            theme={adminTheme}
-          />
-        )}
-      </PageContextProvider>
+      />
     )
   }
